d9513fda0984c94ca1d1b1caaa397ef297a151af
aidan-harrison-git <aidan.harrison@students.fhu.edu>
2024-02-01 12:58:01 -0600

    main.py
    first try

--- main.py
+++ main.py
@@ -2,15 +2,43 @@
 #as a string
 #January -> 1 (as a string)
 #YOU MAY USE THIS FUNCTION IF YOU WANT TO OR YOU MAY REMOVE IT
-def parse_month(month):
-    pass
+def parse_month(raw_month):
+    monthdict = {'January':'01',
+                 'February':'02',
+                 'March':'03',
+                 'April':'04',
+                 'May':'05',
+                 'June':'06',
+                 'July':'07',
+                 'August':'08',
+                 'September':'09',
+                 'October':'10',
+                 'November':'11',
+                 'December':'12'
+                }
+    month_num = monthdict[raw_month]
+    return month_num
 
 #REMOVE PASS AND FIX THIS FUNCTION
 #parse_date function should return the date formated as MM/DD/YYYY
 #DO NOT REMOVE THIS FUNCTION
 def parse_date(user_string):
-    pass
+    token1 = user_string.split()
+    raw_month = token1[0]
+    raw_day = token1[1]
+    if int(raw_day) <=10:
+        day = '0' + str(raw_day[0])
+    else:
+        day = str(raw_day[0:2])
+    
+    component_list = []
+    component_list.append(parse_month(raw_month))
+    component_list.append(day)
+    component_list.append(token1[2])
+    print('/'.join(component_list))
+
 
 #REMOVE PASS AND YOUR CODE GOES HERE
 if __name__ == '__main__':
-    pass
+    user_string = input()
+    parse_date(user_string)
